4d0d066e5199a1ec76f9e463d4844b317b033a26
Januar Harianto <januar.harianto@sydney.edu.au>
2022-10-17 17:12:03 +1100

    tests: add fixtures to conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,4 +7,32 @@
     - https://docs.pytest.org/en/stable/writing_plugins.html
 """
 
-# import pytest
+import ee
+import pytest
+
+
+@pytest.fixture(scope="session", autouse=True)
+def coords():
+    """Fixture for a set of coordinates"""
+    return [149.769345, -30.335861, 149.809173, -30.296271]
+
+
+@pytest.fixture(scope="session", autouse=True)
+def geom_aoi(coords):
+    """Fixture coordinates converted to an ee.Geometry object"""
+    return ee.Geometry.Rectangle(coords)
+
+
+@pytest.fixture(scope="session", autouse=True)
+def ee_imagecollection(coords):
+    """Fixture for an ee.ImageCollection"""
+    ee.Initialize()
+    img = ee.ImageCollection("LANDSAT/LC08/C02/T1_L2")
+    img = img.filterDate("2019-01-01", "2019-02-01")
+    return img.filterBounds(ee.Geometry.Rectangle(coords))
+
+
+@pytest.fixture(scope="session", autouse=True)
+def ee_image(ee_imagecollection):
+    """Fixture for an ee.Image"""
+    return ee_imagecollection.first()
