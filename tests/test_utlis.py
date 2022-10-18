fa4e396dce9244ba01ea79d8f67dee0848736865
Januar Harianto <januar.harianto@sydney.edu.au>
2022-10-18 12:46:29 +1100

    tests: test get_indices

--- tests/test_utlis.py
+++ tests/test_utlis.py
@@ -10,3 +10,11 @@ def test_suppress_stdout(capsys):
         print("This should not be printed")
     captured = capsys.readouterr()
     assert "This should not be printed" not in captured.out
+
+
+def test_get_indices():
+    """
+    Test that the get_indices function downloads the list of indices from
+    Awesome Spectral Indices and prints them as a dict
+    """
+    assert type(utils.get_indices()) is dict
