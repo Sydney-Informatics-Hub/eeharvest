6619042819f70d256f30875b0b2e70666fd84366
Januar Harianto <januar.harianto@sydney.edu.au>
2022-10-18 15:53:02 +1100

    build: remove unused test files

--- tests/test_comm.py
+++
@@ -1,7 +0,0 @@
-from eeharvest import comm
-
-
-def test_msg_info(capsys):
-    comm.msg_info("The 7-th Fibonacci number is 13")
-    captured = capsys.readouterr()
-    assert "Fibonacci" in captured.out
--- tests/test_verify.py
+++
@@ -1,5 +0,0 @@
-from eeharvest import verify
-
-
-def test_verify():
-    assert verify.result() == 3
