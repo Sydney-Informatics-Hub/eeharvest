aeed2fb71ad0f58202b4a84f671c5efbb902a93b
Januar Harianto <januar.harianto@sydney.edu.au>
2022-11-09 13:25:05 +1100

    tests: collect produces error if minimum parameters are provided

---
+++ tests/test_harvester.py
@@ -0,0 +1,27 @@
+import pytest
+
+from eeharvest import harvester
+
+
+def test_collect_produces_ValueError_when_minimum_args_not_provided():
+    with pytest.raises(ValueError) as excinfo:
+        harvester.collect(
+            collection="LANDSAT/LC08/C02/T1_L2",
+            coords=[149.79, -30.31, 149.80, -30.30],
+            # date_min="2019-01-01",
+        )
+    assert "Missing" in str(excinfo.value)
+    with pytest.raises(ValueError) as excinfo:
+        harvester.collect(
+            collection="LANDSAT/LC08/C02/T1_L2",
+            # coords=[149.79, -30.31, 149.80, -30.30],
+            date_min="2019-01-01",
+        )
+    assert "Missing" in str(excinfo.value)
+    with pytest.raises(ValueError) as excinfo:
+        harvester.collect(
+            # collection="LANDSAT/LC08/C02/T1_L2",
+            coords=[149.79, -30.31, 149.80, -30.30],
+            date_min="2019-01-01",
+        )
+    assert "Missing" in str(excinfo.value)
