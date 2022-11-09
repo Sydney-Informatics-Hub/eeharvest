09d6b6639e63389b2b8d5d0c58b2ab63ead66ecb
Januar Harianto <januar.harianto@sydney.edu.au>
2022-11-09 12:37:08 +1100

    tests: validate_schema reads yaml file correctly

--- tests/test_settings.py
+++ tests/test_settings.py
@@ -6,3 +6,10 @@ def test_read_config_works_and_can_be_indexed():
     config = settings.read("configs/template.yaml")
     assert config is not None  # config exists
     assert config["colname_lat"] == "Lat"  # config can be indexed
+
+
+def test_validate_schema_works_on_yaml(capsys):
+    """Test that the config file can be validated against a schema"""
+    settings.validate_schema("configs/template.yaml")
+    captured = capsys.readouterr()
+    assert "validated" in captured.out
