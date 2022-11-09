31027507e271bed4e0f3469123479906c0c4b9c8
Januar Harianto <januar.harianto@sydney.edu.au>
2022-11-09 19:48:28 +1100

    tests: make config file path separate in the code

--- tests/test_settings.py
+++ tests/test_settings.py
@@ -3,7 +3,9 @@ from eeharvest import settings
 
 def test_read_config_works_and_can_be_indexed():
     """Test that the config file can be read"""
-    config = settings.read("configs/template.yaml")
+    # configfile = files("eeharvest.data").joinpath("template.yaml")
+    configfile = "configs/template.yaml"
+    config = settings.read(configfile)
     assert config is not None  # config exists
     assert config["colname_lat"] == "Lat"  # config can be indexed
 
