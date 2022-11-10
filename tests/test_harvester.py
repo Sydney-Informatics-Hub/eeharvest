625dfd310dd6695399a6a4bd2352cf7c5cc99e2b
Januar Harianto <januar.harianto@sydney.edu.au>
2022-11-10 11:52:20 +1100

    tests: collect works with minimum number of arguments

--- tests/test_harvester.py
+++ tests/test_harvester.py
@@ -22,3 +22,14 @@ def test_collect_produces_ValueError_when_minimum_args_not_provided():
             date_min="2019-01-01",
         )
     assert "Missing" in str(excinfo.value)
+
+
+def test_collect_works_with_minimum_args():
+    try:
+        harvester.collect(
+            collection="LANDSAT/LC08/C02/T1_L2",
+            coords=[149.799, -30.31, 149.80, -30.309],
+            date_min="2019-01-01",
+        )
+    except Exception as e:
+        assert False, f"'collect' raised an exception {e}"
