140876d64edaaff009c37ff018393bb47986b022
Januar Harianto <januar.harianto@sydney.edu.au>
2022-11-10 16:54:46 +1100

    tests: clean up code and reduce size of bounding box for testing

--- tests/test_harvester.py
+++ tests/test_harvester.py
@@ -7,21 +7,18 @@ def test_collect_produces_ValueError_when_minimum_args_not_provided():
     with pytest.raises(ValueError) as excinfo:
         harvester.collect(
             collection="LANDSAT/LC08/C02/T1_L2",
-            coords=[149.79, -30.31, 149.80, -30.30],
-            # date_min="2019-01-01",
+            coords=[149.799, -30.31, 149.80, -30.309],
         )
     assert "Missing" in str(excinfo.value)
     with pytest.raises(ValueError) as excinfo:
         harvester.collect(
             collection="LANDSAT/LC08/C02/T1_L2",
-            # coords=[149.79, -30.31, 149.80, -30.30],
             date_min="2019-01-01",
         )
     assert "Missing" in str(excinfo.value)
     with pytest.raises(ValueError) as excinfo:
         harvester.collect(
-            # collection="LANDSAT/LC08/C02/T1_L2",
-            coords=[149.79, -30.31, 149.80, -30.30],
+            coords=[149.799, -30.31, 149.80, -30.309],
             date_min="2019-01-01",
         )
     assert "Missing" in str(excinfo.value)
