c86b10c97ccb2b3ac4dce338abf1075baf7c9e24
Januar Harianto <januar.harianto@sydney.edu.au>
2022-11-18 10:49:12 +1100

    tests: test Google Earth Engine authentication

---
+++ tests/test_auth.py
@@ -0,0 +1,20 @@
+# At some point, check
+# https://gis.stackexchange.com/questions/377222/creating-automated-tests-using-google-earth-engine-python-api
+import sys
+
+# https://stackoverflow.com/a/67246674
+
+
+def test_that_repeat_authentication_is_not_required(capsys):
+    """Test that repeat authentication is not required."""
+    for key in list(sys.modules.keys()):
+        if key.startswith("eeharvest"):
+            del sys.modules[key]
+    from eeharvest import auth
+
+    auth.initialise()
+    captured = capsys.readouterr()
+    assert "authenticated" in captured.out
+    auth.initialise()
+    captured = capsys.readouterr()
+    assert "already authenticated" in captured.out
