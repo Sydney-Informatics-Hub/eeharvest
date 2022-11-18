4651b2a92f9d0feaab39192b6ec6ed0c250c88f7
Januar Harianto <januar.harianto@sydney.edu.au>
2022-11-18 12:43:12 +1100

    tests: change default paths to data

--- tests/test_settings.py
+++ tests/test_settings.py
@@ -1,23 +1,24 @@
+import pytest
 import yaml
 
 from eeharvest import settings
 
 
-def test_read_config_works_and_can_be_indexed(data_path):
+def test_read_config_works_and_can_be_indexed():
     """Test that the config file can be read"""
     # configfile = files("eeharvest.data").joinpath("template.yaml")
-    configfile = data_path.joinpath("template.yaml")
+    configfile = "tests/data/template.yaml"
     config = settings.read(configfile)
     assert config is not None  # config exists
     assert config["colname_lat"] == "Lat"  # config can be indexed
 
 
-def test_validate_schema_validates_error_free_file_with_schema_file(capsys, data_path):
+def test_validate_schema_validates_error_free_file_with_schema_file(capsys):
     """
     validate_schema: should validate a config file with a schema file
     """
-    configfile = data_path.joinpath("template.yaml")
-    schemafile = data_path.joinpath("schema.yaml")
+    configfile = "tests/data/template.yaml"
+    schemafile = "tests/data/schema.yaml"
     settings.validate_schema(configfile, schemafile)
     captured = capsys.readouterr()
     assert "validated" in captured.out
