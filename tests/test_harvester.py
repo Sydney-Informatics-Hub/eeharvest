17c588950fdf3a59e09cc1fb593b783dcc3a5e4a
Januar Harianto <januar.harianto@sydney.edu.au>
2022-11-23 08:08:58 +1100

    tests: add missing imports and cleanup

--- tests/test_harvester.py
+++ tests/test_harvester.py
@@ -1,28 +1,11 @@
+import os
+import os.path
+
 # trunk-ignore(flake8/F401)
 import eemont
 import pytest
 
 from eeharvest import auth, harvester
-
-# def test_collect_produces_ValueError_when_minimum_args_not_provided():
-#     with pytest.raises(ValueError) as excinfo:
-#         harvester.collect(
-#             collection="LANDSAT/LC08/C02/T1_L2",
-#             coords=[149.799, -30.31, 149.80, -30.309],
-#         )
-#     assert "Missing" in str(excinfo.value)
-#     with pytest.raises(ValueError) as excinfo:
-#         harvester.collect(
-#             collection="LANDSAT/LC08/C02/T1_L2",
-#             date_min="2019-01-01",
-#         )
-#     assert "Missing" in str(excinfo.value)
-#     with pytest.raises(ValueError) as excinfo:
-#         harvester.collect(
-#             coords=[149.799, -30.31, 149.80, -30.309],
-#             date_min="2019-01-01",
-#         )
-#     assert "Missing" in str(excinfo.value)
 
 
 def test_collect_stops_when_minimum_args_not_provided(capsys):
