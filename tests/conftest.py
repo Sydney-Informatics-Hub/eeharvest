0cac1b68d1604432bf294a23b0bceda0bc166e28
Januar Harianto <januar.harianto@sydney.edu.au>
2023-01-11 13:54:09 +1100

    chore: cleanup comments

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,15 +1,3 @@
-"""
-    Dummy conftest.py for eeharvest.
-
-    If you don't know what this is for, just leave it empty.
-    Read more about conftest.py under:
-    - https://docs.pytest.org/en/stable/fixture.html
-    - https://docs.pytest.org/en/stable/writing_plugins.html
-"""
-
-
-from __future__ import unicode_literals
-
 import os
 from distutils import dir_util
 from pathlib import Path
